5f55d638082e5f966554f7b65d0dce4494923bc2
julian <mueller@lotum.de>
2022-06-24 14:15:56 +0200

    set up base project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Core from './components/Core.vue'
+</script>
+
+<template>
+  <Core />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Core.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="core">
+        Hallo :)
+    </div>
+</template>
+
+<style scoped>
+.core {
+    color: black;
+}
+</style>
