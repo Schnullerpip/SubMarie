90e8e0c2dd5a5eb383a1a4f110c26d82e2c15ed2
Garrelt Mock <mock@lotum.de>
2022-06-24 22:12:19 +0200

    add svg loader

--- src/components/Core.vue
+++ src/components/Core.vue
@@ -6,6 +6,7 @@ import { BellControls } from "../controls/bellControls";
 import { UserInputHandler } from "../controls/userInput";
 import { Camera } from "../utils/camera";
 import SubMarine from "../assets/SubMarine.png";
+import { loadSvgVertices } from "../utils/svgs";
 
 // module aliases
 const Engine = Matter.Engine,
@@ -55,11 +56,6 @@ userInputHandler.registerCallback((input) => bellControls.addForce(input));
 let render: Render;
 
 onMounted(() => {
-    //convert SVGs to Bodies
-    const bodies = Array.from(document.querySelectorAll<SVGPathElement>("svg > path")).map((path) => {
-        return Matter.Bodies.fromVertices(center.x, center.y, [Matter.Svg.pathToVertices(path, 15)], {}, true);
-    });
-
     // create a renderer
     render = Render.create({
         element: document.querySelector<HTMLElement>("#core")!,
@@ -72,15 +68,24 @@ onMounted(() => {
             showPerformance: true,
             showInternalEdges: true,
             wireframes: false,
+            showBounds: true,
             ...appDimensions,
         },
     });
 
-    const camera = new Camera(bell, engine, render, appDimensions);
+    new Camera(bell, engine, render, appDimensions);
 
     // create ground + left and right mock terrain
-    const boxA = Bodies.rectangle(0, appDimensions.height, 80, appDimensions.height, { isStatic: true });
-    const boxB = Bodies.rectangle(appDimensions.width, appDimensions.height, 80, appDimensions.height, {
+    const background = Bodies.rectangle(center.x, center.y, appDimensions.width, appDimensions.height, {
+        collisionFilter: {
+            group: -2,
+            category: 2,
+            mask: 0,
+        },
+        render: { fillStyle: "lightblue" },
+    });
+    const wallLeft = Bodies.rectangle(0, appDimensions.height, 80, appDimensions.height, { isStatic: true });
+    const wallRight = Bodies.rectangle(appDimensions.width, appDimensions.height, 80, appDimensions.height, {
         isStatic: true,
     });
     const ground = Bodies.rectangle(appDimensions.width / 2, appDimensions.height, appDimensions.width, 60, {
@@ -88,7 +93,7 @@ onMounted(() => {
     });
 
     // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground, bell, ...bodies]);
+    Composite.add(engine.world, [background, wallLeft, wallRight, ground, bell]);
 
     // run the renderer
     Render.run(render);
@@ -98,17 +103,15 @@ onMounted(() => {
 
     // run the engine
     Runner.run(runner, engine);
+
+    loadSvgVertices("/svg/Mediamodifier-Design.svg", 4, center.x, center.y, []).then((svg) =>
+        Composite.add(engine.world, svg)
+    );
 });
 </script>
 
 <template>
     <div id="core"></div>
-
-    <!-- SVG Assets
-    <svg style="display: none" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="1080" height="1080" viewBox="0 0 1080 1080" xml:space="preserve">
-        <path style="stroke: rgb(0,0,0); stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(101,101,101); fill-rule: nonzero; opacity: 1;" vector-effect="non-scaling-stroke"  transform=" translate(-50.5, -50)" d="M 22.66 19.967 C 17.585 19.967 13.448 24.104 13.448 29.180999999999997 L 13.448 78.31 C 13.448 83.387 17.585 87.584 22.723 87.584 L 71.791 87.584 C 76.868 87.584 81.065 83.387 81.065 78.31 C 80.814 71.479 63.769999999999996 68.974 51.613 63.646 L 85.765 29.494999999999997 C 88.146 27.051 88.146 23.165 85.765 20.784 L 79.187 14.203999999999999 C 76.803 11.822999999999999 72.919 11.822999999999999 70.538 14.203999999999999 L 36.76 47.981 C 31.998 36.137 29.616 20.846 22.66 19.967 z" stroke-linecap="round" />
-    </svg>
-    -->
 </template>
 
 <style>
