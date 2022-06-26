93ec64a4a0033a4c051b9dd00b4c6ec4b4463a4d
Garrelt Mock <mock@lotum.de>
2022-06-26 11:32:06 +0200

    new terrain + bg + debug tools

--- src/controls/healthBar.ts
+++ src/controls/healthBar.ts
@@ -4,16 +4,19 @@ import { ref } from "vue";
 import { BellControls } from "./bellControls";
 
 export class HealthBarHandler {
-    private readonly maxHealth = 100;
-    private currentHealth = ref(this.maxHealth);
+    private readonly maxHealth = 1_000_000;
+    public currentHealth = ref(this.maxHealth);
     public health = computed(() => this.currentHealth.value / this.maxHealth);
+
+    public breathDepletion = ref(100);
+    public holeDepletion = ref(35);
 
     private listeners: (() => void)[] = [];
 
     constructor(bellControls: BellControls, engine: Engine) {
         Events.on(engine, "afterUpdate", (e) => {
-            this.currentHealth.value -= bellControls.forces.length * 0.0025;
-            this.currentHealth.value -= 0.01;
+            this.currentHealth.value -= this.breathDepletion.value;
+            this.currentHealth.value -= bellControls.forces.length * this.holeDepletion.value;
 
             if (this.currentHealth.value <= 0) {
                 this.currentHealth.value = 0;
